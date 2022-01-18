2ac9ed94955cfc95e3db53271dfa93377fc16581
andrewmoquia <andrewmoquia09@gmail.com>
2022-01-19 00:56:32 +0800

    Removed rate-limit-mongo.

--- src/utilities/limiter.ts
+++ src/utilities/limiter.ts
@@ -1,26 +1,26 @@
 import rateLimit from 'express-rate-limit'
-import MongoStore from 'rate-limit-mongo'
-import { config } from './config'
+// import MongoStore from 'rate-limit-mongo'
+// import { config } from './config'
 
 //Add limit to all request base on this condition
 export const limiter = rateLimit({
-   store: new MongoStore({
-      uri: `${config.DATABASE}`,
-      collectionName: 'serverLimit',
-      expireTimeMs: 15 * 60 * 1000, // 15 minutes
-      errorHandler: console.error.bind(null, 'rate-limit-mongo'),
-   }),
+   // store: new MongoStore({
+   //    uri: `${config.DATABASE}`,
+   //    collectionName: 'serverLimit',
+   //    expireTimeMs: 15 * 60 * 1000, // 15 minutes
+   //    errorHandler: console.error.bind(null, 'rate-limit-mongo'),
+   // }),
    windowMs: 15 * 60 * 1000, // 15 minutes
    max: 10000, // limit each IP to 200 requests per windowMs
 })
 
 export const login = rateLimit({
-   store: new MongoStore({
-      uri: `${config.DATABASE}`,
-      collectionName: 'loginLimit',
-      expireTimeMs: 15 * 60 * 1000, // 15 minutes
-      errorHandler: console.error.bind(null, 'rate-limit-mongo'),
-   }),
+   // store: new MongoStore({
+   //    uri: `${config.DATABASE}`,
+   //    collectionName: 'loginLimit',
+   //    expireTimeMs: 15 * 60 * 1000, // 15 minutes
+   //    errorHandler: console.error.bind(null, 'rate-limit-mongo'),
+   // }),
    windowMs: 10 * 60 * 1000, // 15 minutes
    max: 200, // limit each IP requests per windowMs
    statusCode: 200,
@@ -31,12 +31,12 @@ export const login = rateLimit({
 })
 
 export const verifyEmailLimiter = rateLimit({
-   store: new MongoStore({
-      uri: `${config.DATABASE}`,
-      collectionName: 'verifyEmailLimit',
-      expireTimeMs: 1 * 60 * 1000, // 1 minute
-      errorHandler: console.error.bind(null, 'rate-limit-mongo'),
-   }),
+   // store: new MongoStore({
+   //    uri: `${config.DATABASE}`,
+   //    collectionName: 'verifyEmailLimit',
+   //    expireTimeMs: 1 * 60 * 1000, // 1 minute
+   //    errorHandler: console.error.bind(null, 'rate-limit-mongo'),
+   // }),
    windowMs: 1 * 60 * 1000, // 15 minutes
    max: 10, // limit each IP requests per windowMs
    statusCode: 200,
@@ -47,12 +47,12 @@ export const verifyEmailLimiter = rateLimit({
 })
 
 export const registerLimit = rateLimit({
-   store: new MongoStore({
-      uri: `${config.DATABASE}`,
-      collectionName: 'registerLimit',
-      expireTimeMs: 24 * 60 * 60 * 1000, // 1 minute
-      errorHandler: console.error.bind(null, 'rate-limit-mongo'),
-   }),
+   // store: new MongoStore({
+   //    uri: `${config.DATABASE}`,
+   //    collectionName: 'registerLimit',
+   //    expireTimeMs: 24 * 60 * 60 * 1000, // 1 minute
+   //    errorHandler: console.error.bind(null, 'rate-limit-mongo'),
+   // }),
    windowMs: 24 * 60 * 60 * 1000, // 24 hours
    max: 100, // limit each IP requests per windowMs
    statusCode: 200,
@@ -63,12 +63,12 @@ export const registerLimit = rateLimit({
 })
 
 export const forgotPW = rateLimit({
-   store: new MongoStore({
-      uri: `${config.DATABASE}`,
-      collectionName: 'forgotPasswordLimit',
-      expireTimeMs: 1 * 60 * 60 * 1000, // 1 minute
-      errorHandler: console.error.bind(null, 'rate-limit-mongo'),
-   }),
+   // store: new MongoStore({
+   //    uri: `${config.DATABASE}`,
+   //    collectionName: 'forgotPasswordLimit',
+   //    expireTimeMs: 1 * 60 * 60 * 1000, // 1 minute
+   //    errorHandler: console.error.bind(null, 'rate-limit-mongo'),
+   // }),
    windowMs: 1 * 60 * 60 * 1000, // 24 hours
    max: 100, // limit each IP requests per windowMs
    statusCode: 200,
