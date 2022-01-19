aa18c817985a79cb64bc96fc57fdbab29ecf8928
andrewmoquia <andrewmoquia09@gmail.com>
2022-01-19 16:33:35 +0800

    Added name in transporter.

--- src/utilities/transporter.ts
+++ src/utilities/transporter.ts
@@ -2,6 +2,7 @@ import nodeMailer from 'nodemailer'
 
 export const transporer = nodeMailer.createTransport({
    service: 'gmail',
+   name: 'incumons.heroku.app',
    auth: {
       user: 'heartweb09@gmail.com',
       pass: 'heartweb123',
