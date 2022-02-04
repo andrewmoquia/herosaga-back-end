65003a6fa6646721e239a6379f8d849088dcfa1d
andrewmoquia <andrewmoquia09@gmail.com>
2022-02-04 16:11:21 +0800

    Changed from address.

--- src/utilities/transporter.ts
+++ src/utilities/transporter.ts
@@ -4,10 +4,11 @@ export const transporer = nodeMailer.createTransport({
    host: 'smtp.gmail.com',
    port: 578,
    service: 'gmail',
+   requireTLS: true,
    name: 'incumons.heroku.app',
    auth: {
       user: 'heartweb09@gmail.com',
       pass: 'heartweb123',
    },
-   from: 'heartweb09@gmail.com',
+   from: 'no-reply@gmail.com',
 })
